bbed0cc442667e353a65541454dc6f140e31fffb
irucaVRC <106234358+irucaVRC@users.noreply.github.com>
2023-02-23 20:00:29 +0900

    imageLoader 0.1.0

---
+++ Editor/ImageLoaderEditor.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+using VRC.SDKBase;
+using System;
+using VRC.Udon;
+
+namespace iruca.ImageLoader
+{
+    [CustomEditor(typeof(ImageLoader))]
+    public class ImageLoaderEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            SerializedProperty defaultTexture = serializedObject.FindProperty("defaultTexture");
+            SerializedProperty eventTargetUdon = serializedObject.FindProperty("udonBehaviour");
+            using (new LabelWidthScope(300))
+            {
+                defaultTexture.objectReferenceValue = EditorGUILayout.ObjectField("Image displayed before loading (optional)", defaultTexture.objectReferenceValue, typeof(Texture), false);
+            }
+            EditorGUILayout.Space(10);
+            eventTargetUdon.objectReferenceValue = EditorGUILayout.ObjectField("Udon Behavior (optional)", eventTargetUdon.objectReferenceValue, typeof(UdonBehaviour), true);
+            EditorGUILayout.Space(20);
+            reorderableList.DoLayoutList();
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        ReorderableList reorderableList;
+
+        void OnEnable()
+        {
+            SerializedProperty prop = serializedObject.FindProperty("images");
+            reorderableList = new ReorderableList(serializedObject, prop)
+            {
+                drawHeaderCallback = (rect) => EditorGUI.LabelField(rect, "Image Download"),
+                drawElementCallback = (rect, index, isActive, isFocused) =>
+                {
+                    SerializedProperty element = prop.GetArrayElementAtIndex(index);
+                    rect.height -= 4;
+                    rect.y += 2;
+                    EditorGUI.PropertyField(rect, element);
+                },
+                elementHeightCallback = (index) =>
+                {
+                    return EditorGUI.GetPropertyHeight(prop.GetArrayElementAtIndex(index)) * 2.5f;
+                },
+            };
+        }
+    }
+
+    [CustomPropertyDrawer(typeof(Images))]
+    public class ImageLoaderEditorDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            //元は 1 つのプロパティーであることを示すために PropertyScope で囲む
+            using (new EditorGUI.PropertyScope(position, label, property))
+            {
+                //ラベル領域の幅を調整
+                EditorGUIUtility.labelWidth = 150;
+
+                position.height = EditorGUIUtility.singleLineHeight;
+
+                //各プロパティーの Rect を求める
+                Rect URLRect = new Rect(position)
+                {
+                    y = position.y
+                };
+
+                Rect MaterialRect = new Rect(URLRect)
+                {
+                    y = URLRect.y + EditorGUIUtility.singleLineHeight + 2
+                };
+
+                //各プロパティーの SerializedProperty を求める
+                SerializedProperty URLProperty = property.FindPropertyRelative("imageUrl");
+                SerializedProperty MaterialProperty = property.FindPropertyRelative("material");
+
+                //各プロパティーの GUI を描画
+                URLProperty.stringValue = EditorGUI.TextField(URLRect, "Image URL", URLProperty.stringValue);
+                MaterialProperty.objectReferenceValue = EditorGUI.ObjectField(MaterialRect, "Material", MaterialProperty.objectReferenceValue, typeof(Material), false);
+            }
+        }
+    }
+
+    public sealed class LabelWidthScope : IDisposable
+    {
+        private readonly float m_oldLabelWidth;
+
+        public LabelWidthScope(int labelWidth)
+        {
+            m_oldLabelWidth = EditorGUIUtility.labelWidth;
+            EditorGUIUtility.labelWidth = labelWidth;
+        }
+
+        public void Dispose()
+        {
+            EditorGUIUtility.labelWidth = m_oldLabelWidth;
+        }
+    }
+}
---
+++ ImageLoader.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDK3.Image;
+using VRC.SDKBase;
+using VRC.Udon;
+#if UNITY_EDITOR
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+#endif
+
+namespace iruca.ImageLoader
+{
+#if UNITY_EDITOR && !COMPILER_UDONSHARP
+    [Serializable]
+    public class Images
+    {
+        [SerializeField] public string imageUrl;
+        [SerializeField] public Material material;
+    }
+
+    [InitializeOnLoad]//エディター起動時にコンストラクタが呼ばれるように
+    public static class PlayModeStateChanged
+    {
+        /// <summary>
+        /// コンストラクタ(InitializeOnLoad属性によりエディター起動時に呼び出される)
+        /// </summary>
+        static PlayModeStateChanged()
+        {
+            //playModeStateChangedイベントにメソッド登録
+            EditorApplication.playModeStateChanged += OnChangedPlayMode;
+        }
+
+        //プレイモードが変更された
+        private static void OnChangedPlayMode(PlayModeStateChange state)
+        {
+            if (state == PlayModeStateChange.EnteredEditMode)
+            {
+                ImageLoader imageLoader = Resources.FindObjectsOfTypeAll<ImageLoader>()[0];
+                foreach (var material in imageLoader.materials)
+                {
+                    if (material != null)
+                    {
+                        material.SetTexture("_MainTex", imageLoader.defaultTexture);
+                    }
+                }
+            }
+        }
+    }
+#endif
+
+    [AddComponentMenu("iruca/ImageLoader")]
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class ImageLoader : UdonSharpBehaviour
+    {
+        [SerializeField] VRCUrl[] urls;
+        public Material[] materials;
+
+        [SerializeField] UdonBehaviour udonBehaviour;
+
+#if UNITY_EDITOR && !COMPILER_UDONSHARP
+        [HideInInspector] public List<Images> images = new List<Images>();
+        public Texture defaultTexture;
+
+        private void OnValidate()
+        {
+            List<VRCUrl> urlList = new List<VRCUrl>();
+            List<Material> materialList = new List<Material>();
+
+            foreach (var item in images)
+            {
+                urlList.Add(new VRCUrl(item.imageUrl));
+                materialList.Add(item.material);
+            }
+            urls = urlList.ToArray();
+            materials = materialList.ToArray();
+            foreach (var material in materials)
+            {
+                if(material != null)
+                {
+                    material.SetTexture("_MainTex", defaultTexture);
+                }
+            }
+        }
+#endif
+
+        void Start()
+        {
+            VRCImageDownloader imageDownloader = new VRCImageDownloader();
+            for (int i = 0; i < materials.Length; i++)
+            {
+                imageDownloader.DownloadImage(urls[i], materials[i], udonBehaviour != null ? udonBehaviour : (VRC.Udon.Common.Interfaces.IUdonEventReceiver)this);
+            }
+        }
+    }
+}
